c384058fb6d8cda384cc34a60b5642f85c03efde
laihem.rab@gmail.com <laihem.rab@gmail.com>
2019-12-03 22:27:25 -0500

    Implemented Interfaces and classes for domain core

---
+++ MicroserviceRabbitMQ/MicroserviceRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroserviceRabbit.Domain.Core.Commands;
+using MicroserviceRabbit.Domain.Core.Events;
+
+namespace MicroserviceRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroserviceRabbitMQ/MicroserviceRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroserviceRabbit.Domain.Core.Events
+{
+    public abstract class Message:IRequest<bool>
+    {
+        public string MessageType { get; set; }
+
+        public Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
